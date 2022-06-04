44168f9ddc02441f9af0a1f40b1c91ff107807d0
g123udini <g123udini@gmail.com>
2022-06-04 19:26:29 +0300

    Поставил тернаный оператор вместо ifa, но мне кажется, что читаемость упала, хз

--- js/main.js
+++ js/main.js
@@ -3,10 +3,7 @@ function getRandomInt(firstNum, secondNum) {
 }
 
 function getRandomFloat(firstNum, secondNum, afterCommaNum) {
-  if (firstNum === secondNum) {
-    return firstNum;
-  }
-  return Math.abs(((Math.random() * (firstNum - secondNum + 1) + secondNum).toFixed(afterCommaNum)));
+  return firstNum === secondNum ? firstNum : Math.abs(((Math.random() * (firstNum - secondNum + 1) + secondNum).toFixed(afterCommaNum)));
 }
 
 getRandomInt(2,3);
