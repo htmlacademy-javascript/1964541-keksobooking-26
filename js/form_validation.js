ab171f313faaca8d64dda2c8d9477a3cd3f17dc9
g123udini <g123udini@gmail.com>
2022-06-18 20:25:12 +0300

    Сделал задание

--- js/form_validation.js
+++ js/form_validation.js
@@ -1,4 +1,4 @@
-const form = document.querySelector('.ad-form');
+import {MinPrice} from './data.js';
 
 const GuestRoomsOptions = {
   '1 комната': ['для 1 гостя'],
@@ -6,6 +6,9 @@ const GuestRoomsOptions = {
   '3 комнаты': ['для 1 гостя', 'для 2 гостей', 'для 3 гостей'],
   '100 комнат': ['не для гостей']
 };
+const MAX_PRICE = 100000;
+
+const form = document.querySelector('.ad-form');
 
 const pristine = new Pristine(form, {
   classTo: 'ad-form__element',
@@ -15,6 +18,12 @@ const pristine = new Pristine(form, {
 
 const guestsField = form.querySelector('[name="capacity"]');
 const roomsField = form.querySelector('[name="rooms"]');
+const priceField = form.querySelector('[name="price"]');
+const typeName = form.querySelector('[name="type"]');
+
+function validatePrice(value) {
+  return MinPrice[typeName.value] <= value && value <= MAX_PRICE;
+}
 
 function validateGuests() {
   return GuestRoomsOptions[roomsField.value].includes(guestsField.value);
@@ -24,10 +33,28 @@ function roomGuestsInvalidMessage () {
   return `${roomsField.value} не ${guestsField.value}`;
 }
 
+function priceInvalidMessage () {
+  return `Должно быть от ${MinPrice[typeName.value]} до ${MAX_PRICE}`;
+}
+
 pristine.addValidator(guestsField, validateGuests, roomGuestsInvalidMessage);
 pristine.addValidator(roomsField, validateGuests, roomGuestsInvalidMessage);
+pristine.addValidator(priceField, validatePrice, priceInvalidMessage);
 
 form.addEventListener('submit', (evt) => {
   evt.preventDefault();
   pristine.validate();
 });
+
+const checkIn = form.querySelector('#timein');
+const checkOut = form.querySelector('#timeout');
+
+checkIn.addEventListener('change', () => {
+  checkOut.value = checkIn.value;
+});
+
+typeName.addEventListener('change', () => {
+  priceField.placeholder = MinPrice[typeName.value];
+});
+
+
