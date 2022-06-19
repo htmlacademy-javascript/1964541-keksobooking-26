11e99c07e17c8920291a827bfb572f60c8432b45
Keks <keks@htmlacademy.ru>
2022-06-19 13:46:25 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/form_validation.js
@@ -0,0 +1,33 @@
+const form = document.querySelector('.ad-form');
+
+const GuestRoomsOptions = {
+  '1 комната': ['для 1 гостя'],
+  '2 комнаты': ['для 1 гостя', 'для 2 гостей'],
+  '3 комнаты': ['для 1 гостя', 'для 2 гостей', 'для 3 гостей'],
+  '100 комнат': ['не для гостей']
+};
+
+const pristine = new Pristine(form, {
+  classTo: 'ad-form__element',
+  errorClass: 'ad-form__element--invalid',
+  errorTextParent: 'ad-form__element'
+});
+
+const guestsField = form.querySelector('[name="capacity"]');
+const roomsField = form.querySelector('[name="rooms"]');
+
+function validateGuests() {
+  return GuestRoomsOptions[roomsField.value].includes(guestsField.value);
+}
+
+function roomGuestsInvalidMessage () {
+  return `${roomsField.value} не ${guestsField.value}`;
+}
+
+pristine.addValidator(guestsField, validateGuests, roomGuestsInvalidMessage);
+pristine.addValidator(roomsField, validateGuests, roomGuestsInvalidMessage);
+
+form.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  pristine.validate();
+});
---
+++ 8/js/main.js
@@ -0,0 +1,8 @@
+import './helpers.js';
+import './data.js';
+import './offers.js';
+import {activatePage, deactivatePage} from './form.js';
+import './form_validation.js';
+
+deactivatePage();
+activatePage();
