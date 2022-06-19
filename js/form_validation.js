7d3da78414b4d02d6e9f0068adc6b8399a12a103
g123udini <g123udini@gmail.com>
2022-06-19 21:57:05 +0300

    Все все поправил

--- js/form_validation.js
+++ js/form_validation.js
@@ -1,17 +1,31 @@
 const form = document.querySelector('.ad-form');
 
 const GuestRoomsOptions = {
-  '1 комната': ['для 1 гостя'],
-  '2 комнаты': ['для 1 гостя', 'для 2 гостей'],
-  '3 комнаты': ['для 1 гостя', 'для 2 гостей', 'для 3 гостей'],
-  '100 комнат': ['не для гостей']
+  '1': ['1'],
+  '2': ['1', '2'],
+  '3': ['1', '2', '3'],
+  '100': ['0']
+};
+
+const GuestCount = {
+  '1': 'для 1 гостя',
+  '2': 'для 2 гостей',
+  '3': 'для 3 гостей',
+  '0': 'не для гостей'
+};
+
+const RoomCount = {
+  '1': '1 комната',
+  '2': '2 комнаты',
+  '3': '3 комнаты',
+  '100': '100 комнат'
 };
 
 const pristine = new Pristine(form, {
   classTo: 'ad-form__element',
   errorClass: 'validation__error',
   errorTextParent: 'ad-form__element'
-}, false);
+});
 
 const guestsField = form.querySelector('[name="capacity"]');
 const roomsField = form.querySelector('[name="rooms"]');
@@ -21,11 +35,19 @@ function validateGuests() {
 }
 
 function roomGuestsInvalidMessage () {
-  return `${roomsField.value} не ${guestsField.value}`;
+  return `${RoomCount[roomsField.value]} не ${GuestCount[guestsField.value]}`;
 }
 
 pristine.addValidator(guestsField, validateGuests, roomGuestsInvalidMessage);
 pristine.addValidator(roomsField, validateGuests, roomGuestsInvalidMessage);
+
+roomsField.addEventListener('change', () => {
+  pristine.validate(guestsField);
+});
+
+guestsField.addEventListener('change', () => {
+  pristine.validate(roomsField);
+});
 
 form.addEventListener('submit', (evt) => {
   evt.preventDefault();
