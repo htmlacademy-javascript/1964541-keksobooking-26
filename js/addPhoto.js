03b67644fc451adb5b7e4322959fb5f8512aaf8a
g123udini <g123udini@gmail.com>
2022-07-07 16:56:53 +0300

    Типо сделал рефакторинг

--- js/addPhoto.js
+++ js/addPhoto.js
@@ -1,21 +1,18 @@
-const FILE_TYPES = ['jpg', 'jpeg', 'png'];
 const offerPhotoContainer = document.querySelector('#photo-img');
 const offerPhotoInput = document.querySelector('#images');
 const avatarContainer = document.querySelector('#avatar-img');
 const avatarInput = document.querySelector('#avatar');
 
-function addPhotoPreview(photoInput, photoContainer, fileTypes) {
+function addPhotoPreview(photoInput, photoContainer) {
   photoInput.addEventListener('change', () => {
     const photoFile = photoInput.files[0];
-    const photoFileName = photoFile.name.toLowerCase();
 
-    const isPhotoFile = fileTypes.some((item) => photoFileName.endsWith(item));
-
-    if(isPhotoFile) {
+    if (photoFile.type.includes('image')) {
       photoContainer.src = URL.createObjectURL(photoFile);
     }
+
   });
 }
 
-addPhotoPreview(avatarInput,avatarContainer, FILE_TYPES);
-addPhotoPreview(offerPhotoInput, offerPhotoContainer, FILE_TYPES);
+addPhotoPreview(avatarInput, avatarContainer);
+addPhotoPreview(offerPhotoInput, offerPhotoContainer);
